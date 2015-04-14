fcf6b2c5b1b4dad9f6a8a261c08101a5d59c888c
Matt Downs <matthewrdowns@gmail.com>
2015-04-13 22:52:44 -0400

    fixed js errors causing tab issues

--- ea/assets/js/backend_reports.js
+++ ea/assets/js/backend_reports.js
@@ -2,7 +2,6 @@ var BackendReports = {
     /**
      * Contains the basic record methods for the page.
      * 
-     * @type ServicesHelper|CategoriesHelper
      */
     helper: {},
     
@@ -14,33 +13,14 @@ var BackendReports = {
      */
     initialize: function(bindEventHandlers) {
         if (bindEventHandlers === undefined) bindEventHandlers = true;
-        
-        // Fill available service categories listbox.
-        // $.each(GlobalVariables.categories, function(index, category) {
-        //     var option = new Option(category.name, category.id);
-        //     $('#service-category').append(option);
-        // });
-        // $('#service-category').append(new Option('- ' + EALang['no_category'] + ' -', null)).val('null');
-        
-        // $('#service-duration').spinner({
-        //     'min': 0,
-        //     'disabled': true //default
-        // });
-        
-        // Instantiate helper object (service helper by default).
-        BackendReports.helper = new ServicesHelper();
-        BackendReports.helper.resetForm();
-        BackendReports.helper.filter('');
-        
-        // $('#filter-services .results').jScrollPane();
-        // $('#filter-categories .results').jScrollPane();
-        
+        $('#historic').hide();
+
         if (bindEventHandlers) BackendReports.bindEventHandlers();        
     },
         
     /**
-     * Binds the default event handlers of the backend services page. Do not use this method
-     * if you include the "BackendServices" namespace on another page.
+     * Binds the default event handlers of the backend reports page. Do not use this method
+     * if you include the "BackendReports namespace on another page.
      */
     bindEventHandlers: function() {
         /**
@@ -53,22 +33,14 @@ var BackendReports = {
             $(this).addClass('active');
             $('.tab-content').hide();
             
-            if ($(this).hasClass('current-tab')) { // display services tab
+            if ($(this).hasClass('current-tab')) {          // display current tab
                 $('#current').show();
-                BackendReports..helper = new ServicesHelper();
-            } else if ($(this).hasClass('historic-tab')) { // display categories tab
+            } else if ($(this).hasClass('historic-tab')) {  // display historic tab
                 $('#historic').show();
-                BackendReports.helper = new CategoriesHelper();
             }
             
-            BackendReports.helper.resetForm();
-            BackendReports.helper.filter('');
             $('.filter-key').val('');
-            //Backend.placeFooterToBottom();
         });
-        
-        // ServicesHelper.prototype.bindEventHandlers();
-        // CategoriesHelper.prototype.bindEventHandlers();
-        
     }
+
 };
